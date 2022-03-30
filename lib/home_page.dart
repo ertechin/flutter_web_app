ca3463b240f70f0a174fdf5f09b641c26f7d32ec
Kekik <kekik@Kekik-Mac-mini.local>
2022-03-30 15:59:50 +0300

    init tabBar

---
+++ lib/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('welcome'),
+            bottom: const TabBar(tabs: [
+              Tab(icon: Icon(Icons.developer_mode), text: 'projects'),
+              Tab(icon: Icon(Icons.newspaper), text: 'blogs'),
+              Tab(icon: Icon(Icons.person), text: 'about me'),
+            ]),
+          ),
+          body: const TabBarView(
+            children: [
+              Center(child: Text('soon')),
+              Center(child: Text('soon')),
+              Center(child: Text('soon')),
+            ],
+          )),
+    );
+  }
+}
