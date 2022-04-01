ad47a28684fcccd93ae2f3695a50761013a4e01b
Kekik <kekik@192.168.1.44>
2022-04-01 15:22:40 +0300

    init dialog/formbuilder, dialog logic

--- lib/controller/blog_page_controller.dart
+++ lib/controller/blog_page_controller.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 
 class BlogPageController extends GetxController {
+  final _formKey = GlobalKey<FormBuilderState>();
   @override
   void onReady() {
     Get.defaultDialog(
       title: 'give me your e-mail !',
-      content: const TextField(),
+      content: FormBuilder(
+        key: _formKey,
+        child: FormBuilderTextField(
+          name: 'e-mail',
+          decoration: const InputDecoration(
+              hintText: 'if i can, believe me i will sell it'),
+        ),
+      ),
       confirm: TextButton(
-          onPressed: () {},
+          onPressed: () {
+            _formKey.currentState!.save();
+            print(_formKey.currentState!.value);
+            Get.back(closeOverlays: true);
+          },
           child: const Text('yes, I already gave it to everyone.')),
     );
     super.onReady();
